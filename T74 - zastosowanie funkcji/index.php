4b550cfdbac5a4b4784bc6b31d19c0f91da87450
Eva-EZ <kittycorn123ewaz@gmail.com>
2026-09-23 21:55:28 +0200

    rfdffff

--- T74 - zastosowanie funkcji/index.php
+++ T74 - zastosowanie funkcji/index.php
@@ -1,5 +1,7 @@
 <?php
 echo "<b>T741</b> <br>";
+
+/*
 function wbz1 ($x){
     if ($x > 0) {
         return $x;
@@ -17,3 +19,83 @@ echo wbz1(-3) . "\n <br>";
 echo wbz1(7) . "\n <br>";
 echo wbz2(15) . "\n <br>";
 echo wbz2(-21) . "\n <br>";
+
+
+function potega($x, $y){
+    $wynik = $x ** $y;
+    echo "x = $x <br> y = $y <br> x<sup>y</sup> = $x<sup>$y</sup> = ";
+    return $wynik;
+}
+
+echo potega(4,2);
+
+*/
+
+/*
+function miesiece($liczba){
+    if ($liczba == 1){
+        return "styczen";
+    }elseif ($liczba == 2){
+        return "luty";
+    }elseif ($liczba == 3){
+        return "marzec";
+    }elseif ($liczba == 4){
+        return "kwiecień";
+    }elseif ($liczba == 5){
+        return "maj";
+    }elseif ($liczba == 6){
+        return "czerwiec";
+    }elseif ($liczba == 7){
+        return "lipiec";
+    }elseif ($liczba == 8){
+        return "sierpień";
+    }elseif ($liczba == 9){
+        return "wrzesień";
+    }elseif ($liczba == 10){
+        return "październik";
+    }elseif ($liczba == 11){
+        return "listopad";
+    }elseif ($liczba == 12){
+        return "grudzień";
+    }else{
+        return "błąd";
+    }
+}
+
+echo miesiece(4);
+echo miesiece(0);
+
+*/
+/*
+function mniejsza_wieksza_rowna($a, $b){
+    if ($a > $b) {
+        return "<font color='red'>$a</font> > <font color='green'>$b</font>";
+    }elseif ($a < $b) {
+        return "<font color='green'>$a</font> < <font color='red'>$b</font>";
+    }else{
+        return "<font color='blue'>$a</font> = <font color='blue'>$b</font>";
+    }
+}
+
+echo mniejsza_wieksza_rowna(2, 67);
+echo "<br>";
+echo mniejsza_wieksza_rowna(69, 67);
+echo "<br>";
+echo mniejsza_wieksza_rowna(69,69);
+
+*/
+/*
+function eh_ten_euklides($a, $b){
+    while ($b != 0){
+        $r = $a % $b;
+        $a = $b;
+        $b = $r;
+    }
+    return $a;
+}
+
+echo eh_ten_euklides(4, 6);
+echo "<br>";
+echo eh_ten_euklides(4, 0);
+
+*/
